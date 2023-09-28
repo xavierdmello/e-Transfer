ff5337ad9b1e2001a82b9654da1bfdab08b333e4
Xavier D'Mello <xaviermdmello@gmail.com>
2023-09-28 15:22:40 -0400

    remove port thingu

--- backend/server.ts
+++ backend/server.ts
@@ -13,7 +13,7 @@ const POSTMARK_KEY = process.env.POSTMARK_KEY!;
 const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 const eTransferAddress = "0xB2D2f29e572577854306099DFA24B07596eC92a7";
 const tokenAddress = "0x62e6940856c42bD23C0c895824921678A37A62aE";
-const PORT ="8080";
+
 let client = new postmark.ServerClient(POSTMARK_KEY);
 
 // Order of events:
