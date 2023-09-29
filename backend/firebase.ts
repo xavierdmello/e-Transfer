e93968bff02bbe2b80d667e5fd60adb54bf05caf
Xavier D'Mello <xaviermdmello@gmail.com>
2023-09-28 21:18:52 -0400

    update db

--- backend/firebase.ts
+++ backend/firebase.ts
@@ -2,7 +2,7 @@ import { getDatabase, ref, set, onValue, onChildAdded } from "firebase/database"
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  databaseURL: "https://e-transfer-daa7e-default-rtdb.firebaseio.com/",
+  databaseURL: "https://e-transfer-prod-default-rtdb.firebaseio.com/",
 };
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
