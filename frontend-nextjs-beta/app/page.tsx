69a21537e417f536443ad25d1469335867d9ba97
Xavier D'Mello <xaviermdmello@gmail.com>
2023-10-15 19:29:30 -0400

    da

--- frontend-nextjs-beta/app/page.tsx
+++ frontend-nextjs-beta/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return <main>dsad</main>;
 }
